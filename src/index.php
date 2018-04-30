26e709ff7b84d7bb060a07c9c5fabcaee4ac4b17
AL <free.ukrainev@gmail.com>
2018-04-30 22:57:30 +0300

    lab_1.2.2.2

--- src/index.php
+++ src/index.php
@@ -1,8 +1,42 @@
 <?php
 
-$stringName = 'Hello';
-$boleanName = true;
-$intName = 5;
-$integerName = 5.5;
-$arrayName = [4 ,4];
-$variableName = 'Hello';
+/*
+    (int), (integer) - приведение к integer
+    (bool), (boolean) - приведение к boolean
+    (float), (double), (real) - приведение к float
+    (string) - приведение к string
+    (array) - приведение к array
+    (object) - приведение к object
+    (unset) - приведение к NULL
+*/
+
+/*
+o Boolean to Integer и наоборот (для целочисленного ученика следует попробовать преобразовать в bool положительные, отрицательные и нулевые числа)
+o Float to Integer и наоборот
+o String to Integer и наоборот (для строк ученик должен попробовать преобразовать строки с номерами в контент)
+o Массив для строки и наоборот
+o Массив к объекту и обратно
+• Студент может попытаться преобразовать другие типы переменных*/
+
+
+
+$variableName = (float)'5';
+$variableName = (int)$variableName;
+
+$variableName = 5;
+$variableName = (float)$variableName;
+$variableName = (double)$variableName;
+
+
+$variableName = '5';
+$variableName = (int)$variableName;
+
+$variableName = '5';
+$variableName = (array)$variableName;
+$variableName = implode($variableName);
+
+$variableName = '5';
+$variableName = (object)$variableName;
+$variableName = (array)$variableName;
+
+var_dump ($variableName);
