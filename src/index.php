74f5e7190c983190fad6761518c0cec6fca7c255
AL <free.ukrainev@gmail.com>
2018-04-30 23:24:27 +0300

    lab_1.2.2.3 (Warning)line 3.8 and 4.8

--- src/index.php
+++ src/index.php
@@ -1,42 +1,12 @@
 <?php
 
-/*
-    (int), (integer) - приведение к integer
-    (bool), (boolean) - приведение к boolean
-    (float), (double), (real) - приведение к float
-    (string) - приведение к string
-    (array) - приведение к array
-    (object) - приведение к object
-    (unset) - приведение к NULL
-*/
+$sum = 6 + '5';
+$sum = '5' + 3;
+//$sum = '' + '';
+$sum = (float)5.23 + (integer)3;
+$sum = (1.3 + 0.1 == 1.4);//почему false
 
-/*
-o Boolean to Integer и наоборот (для целочисленного ученика следует попробовать преобразовать в bool положительные, отрицательные и нулевые числа)
-o Float to Integer и наоборот
-o String to Integer и наоборот (для строк ученик должен попробовать преобразовать строки с номерами в контент)
-o Массив для строки и наоборот
-o Массив к объекту и обратно
-• Студент может попытаться преобразовать другие типы переменных*/
+$r = 1.3 + 0.1;
+var_dump($sum);
+echo ($r);
 
-
-
-$variableName = (float)'5';
-$variableName = (int)$variableName;
-
-$variableName = 5;
-$variableName = (float)$variableName;
-$variableName = (double)$variableName;
-
-
-$variableName = '5';
-$variableName = (int)$variableName;
-
-$variableName = '5';
-$variableName = (array)$variableName;
-$variableName = implode($variableName);
-
-$variableName = '5';
-$variableName = (object)$variableName;
-$variableName = (array)$variableName;
-
-var_dump ($variableName);
