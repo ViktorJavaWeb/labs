628813645577a2840e00b3c586090b13f018cc0a
AL <free.ukrainev@gmail.com>
2018-04-30 23:49:03 +0300

    lab_1.2.2.4

--- src/index.php
+++ src/index.php
@@ -1,12 +1,14 @@
 <?php
 
-$sum = 6 + '5';
-$sum = '5' + 3;
-//$sum = '' + '';
-$sum = (float)5.23 + (integer)3;
-$sum = (1.3 + 0.1 == 1.4);//почему false
+$variableName = 5;
+$variableName = "f t 5";
+$variableName = <<<EOT
+    Hello heredoc $variableName
+EOT;
 
-$r = 1.3 + 0.1;
-var_dump($sum);
-echo ($r);
+$variableName = <<<'EOD'
+    Hello nowdoc
+EOD;
 
+
+echo ($variableName);
