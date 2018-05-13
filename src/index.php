3565e413e8efafbbb1a339500e91bf60686f45b7
AL <free.ukrainev@gmail.com>
2018-05-13 22:21:39 +0300

    lab_1.4.1.1

--- src/index.php
+++ src/index.php
@@ -1,7 +1,66 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+function notNull($x, $y)
+{
+    if (!isset($x) or !isset($y)) {
+        if (!isset($x)) {
+            $errx = '$x - пустой';
+
+        }
+        if (!isset($y)) {
+            $erry = '$y - пустой';
+
+        }
+    }
+    return $errx.$erry;
+}
+
+function multiplication($x, $y){
+    if (!notNull($x, $y)){
+        return $x * $y;
+    }else {
+        $err = notNull($x, $y);
+    }
+    return $err;
+}
+
+function division($x, $y){
+    if (!notNull($x, $y)){
+        if($y != 0){
+            return $x / $y;
+        }else {
+            $err = 'На ноль делить нельзя';
+        }
+    }
+    return $err;
+}
+
+function addition($x, $y){
+    if (!notNull($x, $y)){
+        return $x + $y;
+    }else {
+        $err = notNull($x, $y);
+    }
+    return $err;
+}
+
+function substraction($x, $y){
+    if (!notNull($x, $y)){
+        return $x - $y;
+    }else {
+        $err = notNull($x, $y);
+    }
+    return $err;
+}
+
+
+$r = multiplication(4,5);
+echo 'multiplication - ' . $r . '<br>';
+
+$r = division(5,5);
+echo 'division - ' . $r . '<br>';
+
+$r = addition(5,50);
+echo 'addition - ' . $r . '<br>';
+
+$r = substraction(12,60);
+echo 'substraction - ' . $r . '<br>';
