ba4f8ac32cd9bc4c4620c20d062a40ea749da0c6
AL <free.ukrainev@gmail.com>
2018-05-13 19:55:55 +0300

    lab_1.3.9.1

--- src/index.php
+++ src/index.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+function calculateFactorial($x){
+    $z = $x;
+    while($x>1){
+        $y = $x-1;
+        $z = $z * $y;
+        $x = $y;
+
+    }
+    echo 'calculateFactorial <h4> result - '.$z.'</h4>';
+}
+
+calculateFactorial(5);
