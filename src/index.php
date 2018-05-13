c5397a32f9f5f4e247e2321399ba6df3e0c7c7a9
AL <free.ukrainev@gmail.com>
2018-05-13 18:04:22 +0300

    lab_1.3.8

--- src/index.php
+++ src/index.php
@@ -1,12 +1,19 @@
 <?php
 
+function getRandomNumber($x, $y){
+    return rand($x ,$y);
+}
+
 function calculateSum($x, $y){
     echo 'calculateSum - ' . ($x + $y) . '<br>';
+    return $x + $y;
 }
 
 function multiply($x, $y){
-    echo 'multiply - '. ($x * $y);
+    echo 'multiply - '. ($x * $y) . '<br>';
+    return $x * $y;
 }
 
 calculateSum(2,3);
 multiply(2,3);
+echo 'getRandomNumber - ' . getRandomNumber(0, 50);
