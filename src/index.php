d919ce1cdb7cc6fdbeca5c63613b2d23231bed3a
AL <free.ukrainev@gmail.com>
2018-05-14 20:45:48 +0300

    lab_1.4.1.2

--- src/index.php
+++ src/index.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+$students = array(
+    'vik' => array('name' => 'Viktor', 'age' => 25, 'gender' => 'men'),
+    'tetyan' => array('name' => 'Tetyana', 'age' => 30, 'gender' => 'women')
+);
+
+function findStudent($name, $students){
+
+    foreach ($students as $key => $item){
+
+        if($name == $key){
+            foreach ($item as $key2 => $item2){
+                echo $key2 . ' => ' . $item2 .'<br>';
+            }
+            exit();
+        }else{
+            echo "Такого студента несуществует";
+        }
+    }
+}
+
+findStudent('ff', $students);
