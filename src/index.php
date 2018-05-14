95d2fb0cd6ba0386fcd00c239174cdb1ec8b54df
AL <free.ukrainev@gmail.com>
2018-05-15 00:29:12 +0300

    revert index.php

--- src/index.php
+++ src/index.php
@@ -1,43 +1,7 @@
 <?php
-
-/*
-    Monday
-    Tuesday
-    Wednesday
-    Thursday
-    Friday
-    Sunday
-    Saturday
-
-*/
-
-$firstTimeDay = 9; // начало рабочего дня
-$lastTimeDay = 18; // конец рабочего дня
-$time = '8'; // задаваемое время дня
-$dayWeek = 'Monday';
-
-
-//var_dump(empty($time));
-
-
-if (isset($dayWeek) && isset($time)){
-    if (!($dayWeek == 'Sunday' or $dayWeek == 'Saturday') and $time <= 24) {
-        if ($firstTimeDay < $time and $lastTimeDay > $time) {
-            $time = $lastTimeDay - $time;
-            echo "<h4>Количество часов до окончания рабочего дня - $time часа</h4>";
-        } elseif ($time < $firstTimeDay) {
-            $time = $firstTimeDay - $time;
-            echo "<h4>Количество часов до начала рабочего дня - $time часа</h4>";
-        }
-        if ($lastTimeDay < $time) {
-            $time = $time - $lastTimeDay;
-            echo "<h4>Количество часов после окончания рабочего дня - $time часа</h4>";
-        }
-    }elseif ($dayWeek == 'Sunday' or $dayWeek == 'Saturday'){
-        echo "ВЫХОДНЫЕ  !!!!!!!!!!!!!!!!!!!!";
-    }else{
-        echo "WARNING!!!!!!!";
-    }
-}else{
-    echo "WARNING !";
-}
+/**
+ * Created by IntelliJ IDEA.
+ * User: AL
+ * Date: 30.04.2018
+ * Time: 20:46
+ */
