ddf9c3c8daebd595a712ee4488490d8230a18180
AL <free.ukrainev@gmail.com>
2018-05-14 23:03:29 +0300

    lab_1.4.1.3 Warning:(8, 10) Statement has empty body

--- src/index.php
+++ src/index.php
@@ -1,22 +1,36 @@
 <?php
+function findStudent($name, $students){
+    foreach ($students as $key => $item){
+        if($name == $key){
+            foreach ($item as $key2 => $item2){
+                echo $key2 . ' => ' . $item2 .'<br>';
+            }
+        }else{
+           // echo "Такого студента несуществует";
+        }
+    }
+}
+
+function addArray($a, $name, $b){
+    $a = array_merge($a[$name], $b);
+    return $a;
+}
+
+
 $students = array(
     'vik' => array('name' => 'Viktor', 'age' => 25, 'gender' => 'men'),
     'tetyan' => array('name' => 'Tetyana', 'age' => 30, 'gender' => 'women')
 );
 
-function findStudent($name, $students){
+$arr = array('lessons' => array('JAVA'));
+$langProgram = array('java', 'javascript', 'php', 'cu');
 
-    foreach ($students as $key => $item){
+findStudent('vik', $students);
 
-        if($name == $key){
-            foreach ($item as $key2 => $item2){
-                echo $key2 . ' => ' . $item2 .'<br>';
-            }
-            exit();
-        }else{
-            echo "Такого студента несуществует";
+
+foreach ($students as $key => $item){
+        foreach ($item as $key2 => $item2){
+            print_r(addArray($students, $key, $arr));
         }
-    }
 }
-
-findStudent('ff', $students);
+//print_r($students);
