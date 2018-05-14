c2420ecac4a02f266ed501727db98e2f64e97822
AL <free.ukrainev@gmail.com>
2018-05-14 23:30:55 +0300

    lab_1.4.2.1

--- src/index.php
+++ src/index.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+$sequence = array(1,2,3,4,5,6);
+$shuffle = array(5,6,2,3,4,1);
+
+$arrList = $shuffle;
+
+echo 'Number of elements in the array - ' . count($arrList) . '<br><br>';
+
+for ($i = 0; $i < count($arrList); $i++){
+    if($i == 0){
+        echo 'First elements - ' . $arrList[$i] . '<br>';
+    }
+    if($i == count($arrList)-1){
+        echo 'Last elements - ' . $arrList[$i] . '<br>';
+    }
+}
+
+
+
+$varString = 'Lorem ipsum';
+$arrWorld = explode (' ', $varString);
+
+array_push($sequence, $arrWorld);
+$arr = array_merge($arrWorld, $sequence);
+print_r($arr);
