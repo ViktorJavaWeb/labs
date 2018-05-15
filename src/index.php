6a53b474a5989a783773904337aecc1c2e9474aa
AL <free.ukrainev@gmail.com>
2018-05-15 19:03:39 +0300

    lab_1.4.4

--- src/index.php
+++ src/index.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+$students = array(
+    'vik' => array(
+        'name' => 'Viktor',
+        'age' => 30,
+        'gender' => 'men',
+        'language' => 'eng'
+    ),
+    'tatyana' => array(
+        'name' => 'Tatyana',
+        'age' => 25,
+        'gender' => 'women',
+        'language' => 'ukr'
+    )
+);
+
+
+function searchSkills ($students, $language){
+
+    foreach($students as $key){
+
+        foreach($key as $key1 => $item){
+            if($language == $key1){
+                echo 'Hello ' . $key['name'] . ' language - ' . $item . '<br>';
+            }
+        }
+    }
+}
+
+searchSkills($students, 'language');
