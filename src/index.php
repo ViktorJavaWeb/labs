e3c86b1d70a7a7d2ebc56d4e4dc2aa7339fba346
AL <free.ukrainev@gmail.com>
2018-05-15 19:02:26 +0300

    lab_1.4.3.4

--- src/index.php
+++ src/index.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: AL
- * Date: 30.04.2018
- * Time: 20:46
- */
+$arrKey = ['h','P','L','k','Q','R'];
+
+$keyHach = md5('L');
+echo '$keyHach - ' . $keyHach . '<br><br>';
+
+foreach($arrKey as $key => $item){
+    if(md5($item) == $keyHach){
+        echo 'HELLO!!!!';
+    }
+}
